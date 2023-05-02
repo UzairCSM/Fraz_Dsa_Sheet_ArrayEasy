35d31ac705cdeadc9487eda09a02ba3902010fa7
UzairCSM <mohdujairkhan141@gmail.com>
2023-05-02 13:33:37 +0530

    initial commit

--- Merge.java
+++ Merge.java
@@ -3,7 +3,8 @@ import java.util.*;
 public class Merge {
     class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
-           
+            int merged_length=m+n;
+            int[] merged_array=new int[merged_length];
             int pos=0;
             for(int i=0;i<m;i++){
                 merged_array[pos]=nums1[i];
